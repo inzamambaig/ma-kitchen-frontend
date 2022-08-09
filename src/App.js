5fd1a787db0d187defe1ec6691bfd319996fbf10
Inzamam Baig <baigsone@gmail.com>
2022-08-09 19:46:43 +0500

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import { useSelector } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -7,32 +6,30 @@ import HomeChefLogin from "./pages/auth/homeChefLogin/HomeChefLogin";
 import HomeChefSignUp from "./pages/auth/homeChefSignup/HomeChefSignUp";
 import Logout from "./pages/auth/Logout";
 import Dashboard from "./pages/dashboard/Dashboard";
-// import Logout from "./pages/auth/Logout";
 
 function App() {
-  // const { chef } = useSelector((state) => state.auth);
-
   return (
     <>
       <Router>
         <div className="App">
           <Routes>
-            {/* {chef ? (
-              <>
-                <Route exact path="/logout" element={<Logout />} />
-              </>
-            ) : ( */}
-            <>
-              <Route exact path="/" element={<HomeChefLogin />} />
-              <Route path="/signup" element={<HomeChefSignUp />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/logout" element={<Logout />} />
-            </>
-            {/* )} */}
+            <Route exact path="/" element={<HomeChefLogin />} />
+            <Route path="/signup" element={<HomeChefSignUp />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/logout" element={<Logout />} />
           </Routes>
         </div>
       </Router>
-      <ToastContainer />
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        pauseOnHover
+      />
     </>
   );
 }
--- src/pages/home/Home.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return <div>Home sweet home </div>;
-}
